08c3ab35a3f6e3379de96a508230595cb478b92a
Liujiajia1994 <460686664@qq.com>
2019-12-04 21:18:46 +0800

    login test

--- login.py
+++ login.py
@@ -16,24 +16,31 @@ class Ui_widget(object):
     def setupUi(self, widget):
         widget.setObjectName("widget")
         widget.resize(400, 300)
+
         self.pushButton = QtWidgets.QPushButton(widget)
         self.pushButton.setGeometry(QtCore.QRect(200, 190, 75, 23))
         self.pushButton.setObjectName("pushButton")
+
         self.pushButton_2 = QtWidgets.QPushButton(widget)
         self.pushButton_2.setGeometry(QtCore.QRect(80, 190, 75, 23))
         self.pushButton_2.setObjectName("pushButton_2")
+
         self.Label = QtWidgets.QLabel(widget)
         self.Label.setGeometry(QtCore.QRect(81, 51, 24, 16))
         self.Label.setObjectName("Label")
+
         self.LineEdit = QtWidgets.QLineEdit(widget)
         self.LineEdit.setGeometry(QtCore.QRect(182, 51, 128, 20))
         self.LineEdit.setObjectName("LineEdit")
+
         self.Label_2 = QtWidgets.QLabel(widget)
         self.Label_2.setGeometry(QtCore.QRect(81, 90, 24, 16))
         self.Label_2.setObjectName("Label_2")
+
         self.LineEdit_2 = QtWidgets.QLineEdit(widget)
         self.LineEdit_2.setGeometry(QtCore.QRect(182, 90, 128, 20))
         self.LineEdit_2.setObjectName("LineEdit_2")
+
         self.checkBox = QtWidgets.QCheckBox(widget)
         self.checkBox.setGeometry(QtCore.QRect(120, 140, 95, 16))
         self.checkBox.setObjectName("checkBox")
