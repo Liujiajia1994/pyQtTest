186e51c02dcc3ff9033e7ed88762dcefa51df0e4
Liujiajia1994 <460686664@qq.com>
2019-12-30 16:15:06 +0800

    预处理界面调整

--- oceanEddyRecognition.py
+++ oceanEddyRecognition.py
@@ -59,6 +59,32 @@ class Ui_MainWindow(object):
         self.groupBox_3.setObjectName("groupBox_3")
         self.open_file = QtWidgets.QPushButton(self.groupBox_3)
         self.open_file.setGeometry(QtCore.QRect(100, 50, 151, 51))
+        self.open_file.setStyleSheet("QPushButton{  \n"
+"    border: 1px solid #ffffff;    \n"
+"    background-color:#62d8fe;\n"
+"    border-style: solid;  \n"
+"    border-radius:5px;  \n"
+"    width: 100px;  \n"
+"    height:20px;  \n"
+"    padding:0 0px;  \n"
+"} \n"
+"QPushButton:hover{     \n"
+"    border: 1px solid #ffffff;  \n"
+"    background-color:#d6f7fe;\n"
+"    border-style: solid;  \n"
+"    border-radius:5px;  \n"
+"    width: 40px;  \n"
+"    height:20px;  \n"
+"    padding:0 0px;  \n"
+"}\n"
+"QPushButton:pressed{  \n"
+"    background-color:#EAF0FF;  \n"
+"    border: 1px solid #AAB4C4;  \n"
+"    width: 40px;  \n"
+"    height:20px;  \n"
+"    padding:0 0px;  \n"
+"    border-radius:5px;  \n"
+"}")
         self.open_file.setIconSize(QtCore.QSize(16, 16))
         self.open_file.setObjectName("open_file")
         self.verticalLayout.addWidget(self.groupBox_3)
@@ -70,7 +96,6 @@ class Ui_MainWindow(object):
         self.textEdit_info.setObjectName("textEdit_info")
         self.verticalLayout.addWidget(self.groupBox_2)
         self.verticalLayout.setStretch(0, 1)
-        self.verticalLayout.setStretch(1, 1)
         self.horizontalLayout.addLayout(self.verticalLayout)
         self.groupBox = QtWidgets.QGroupBox(self.tab_file)
         self.groupBox.setMinimumSize(QtCore.QSize(349, 481))
@@ -78,76 +103,161 @@ class Ui_MainWindow(object):
         self.image_preshow = QtWidgets.QLabel(self.groupBox)
         self.image_preshow.setGeometry(QtCore.QRect(10, 20, 121, 41))
         self.image_preshow.setObjectName("image_preshow")
-        self.label_show = QtWidgets.QLabel(self.groupBox)
-        self.label_show.setGeometry(QtCore.QRect(10, 110, 321, 280))
-        self.label_show.setMinimumSize(QtCore.QSize(280, 280))
-        self.label_show.setText("")
-        self.label_show.setScaledContents(True)
-        self.label_show.setObjectName("label_show")
         self.comboBox = QtWidgets.QComboBox(self.groupBox)
-        self.comboBox.setGeometry(QtCore.QRect(150, 30, 171, 22))
+        self.comboBox.setGeometry(QtCore.QRect(150, 30, 171, 30))
+        self.comboBox.setStyleSheet("QComboBox {\n"
+"    font-family: \"微软雅黑\";\n"
+"    color: black;\n"
+"    border: 1px #1a1a1a solid;\n"
+"    background-color:#62d8fe;\n"
+"}\n"
+"")
         self.comboBox.setObjectName("comboBox")
+        self.comboBox.addItem("")
+        self.tabWidget_2 = QtWidgets.QTabWidget(self.groupBox)
+        self.tabWidget_2.setGeometry(QtCore.QRect(10, 90, 351, 381))
+        self.tabWidget_2.setObjectName("tabWidget_2")
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab")
+        self.label_show = QtWidgets.QLabel(self.tab)
+        self.label_show.setGeometry(QtCore.QRect(0, 0, 351, 351))
+        self.label_show.setMinimumSize(QtCore.QSize(351, 351))
+        self.label_show.setMaximumSize(QtCore.QSize(351, 351))
+        self.label_show.setScaledContents(True)
+        self.label_show.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_show.setObjectName("label_show")
+        self.tabWidget_2.addTab(self.tab, "")
+        self.tab_2 = QtWidgets.QWidget()
+        self.tab_2.setObjectName("tab_2")
+        self.label_show_gray = QtWidgets.QLabel(self.tab_2)
+        self.label_show_gray.setGeometry(QtCore.QRect(0, 0, 351, 351))
+        self.label_show_gray.setScaledContents(True)
+        self.label_show_gray.setObjectName("label_show_gray")
+        self.tabWidget_2.addTab(self.tab_2, "")
         self.horizontalLayout.addWidget(self.groupBox)
         self.horizontalLayout.setStretch(0, 1)
         self.horizontalLayout.setStretch(1, 1)
         self.gridLayout_2.addLayout(self.horizontalLayout, 0, 0, 1, 1)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("E:/GitHub/pyQtTest/source/folder_opened_128px_1222856_easyicon.net.ico"), QtGui.QIcon.Selected, QtGui.QIcon.On)
+        icon.addPixmap(QtGui.QPixmap("source/folder_opened_128px_1222856_easyicon.net.ico"), QtGui.QIcon.Selected, QtGui.QIcon.On)
         self.tabWidget.addTab(self.tab_file, icon, "")
         self.tab_process = QtWidgets.QWidget()
         self.tab_process.setObjectName("tab_process")
-        self.gridLayout_3 = QtWidgets.QGridLayout(self.tab_process)
-        self.gridLayout_3.setObjectName("gridLayout_3")
-        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.groupBox_4 = QtWidgets.QGroupBox(self.tab_process)
-        self.groupBox_4.setMinimumSize(QtCore.QSize(301, 481))
+        self.widget = QtWidgets.QWidget(self.tab_process)
+        self.widget.setGeometry(QtCore.QRect(10, 10, 753, 488))
+        self.widget.setObjectName("widget")
+        self.horizontalLayout_5 = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_5.setObjectName("horizontalLayout_5")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.groupBox_4 = QtWidgets.QGroupBox(self.widget)
+        self.groupBox_4.setMinimumSize(QtCore.QSize(371, 217))
         self.groupBox_4.setObjectName("groupBox_4")
-        self.checkBox = QtWidgets.QCheckBox(self.groupBox_4)
-        self.checkBox.setGeometry(QtCore.QRect(40, 60, 101, 51))
-        self.checkBox.setObjectName("checkBox")
-        self.checkBox_2 = QtWidgets.QCheckBox(self.groupBox_4)
-        self.checkBox_2.setGeometry(QtCore.QRect(40, 140, 101, 51))
-        self.checkBox_2.setObjectName("checkBox_2")
-        self.checkBox_3 = QtWidgets.QCheckBox(self.groupBox_4)
-        self.checkBox_3.setGeometry(QtCore.QRect(40, 220, 101, 51))
+        self.pushButtoncheck = QtWidgets.QPushButton(self.groupBox_4)
+        self.pushButtoncheck.setGeometry(QtCore.QRect(130, 150, 101, 51))
+        self.pushButtoncheck.setMinimumSize(QtCore.QSize(101, 51))
+        self.pushButtoncheck.setStyleSheet("QPushButton{  \n"
+"    border: 1px solid #ffffff;    \n"
+"    background-color:#62d8fe;\n"
+"    border-style: solid;  \n"
+"    border-radius:5px;  \n"
+"    width: 100px;  \n"
+"    height:20px;  \n"
+"    padding:0 0px;  \n"
+"} \n"
+"QPushButton:hover{     \n"
+"    border: 1px solid #ffffff;  \n"
+"    background-color:#d6f7fe;\n"
+"    border-style: solid;  \n"
+"    border-radius:5px;  \n"
+"    width: 40px;  \n"
+"    height:20px;  \n"
+"    padding:0 0px;  \n"
+"}\n"
+"QPushButton:pressed{  \n"
+"    background-color:#EAF0FF;  \n"
+"    border: 1px solid #AAB4C4;  \n"
+"    width: 40px;  \n"
+"    height:20px;  \n"
+"    padding:0 0px;  \n"
+"    border-radius:5px;  \n"
+"}")
+        self.pushButtoncheck.setObjectName("pushButtoncheck")
+        self.widget1 = QtWidgets.QWidget(self.groupBox_4)
+        self.widget1.setGeometry(QtCore.QRect(50, 30, 248, 78))
+        self.widget1.setObjectName("widget1")
+        self.gridLayout_3 = QtWidgets.QGridLayout(self.widget1)
+        self.gridLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.checkBox_3 = QtWidgets.QCheckBox(self.widget1)
+        self.checkBox_3.setMinimumSize(QtCore.QSize(120, 35))
         self.checkBox_3.setObjectName("checkBox_3")
-        self.horizontalLayout_2.addWidget(self.groupBox_4)
-        self.pushButton_check = QtWidgets.QPushButton(self.tab_process)
-        self.pushButton_check.setMinimumSize(QtCore.QSize(101, 51))
-        self.pushButton_check.setObjectName("pushButton_check")
-        self.horizontalLayout_2.addWidget(self.pushButton_check)
-        self.groupBox_5 = QtWidgets.QGroupBox(self.tab_process)
-        self.groupBox_5.setMinimumSize(QtCore.QSize(331, 481))
+        self.gridLayout_3.addWidget(self.checkBox_3, 0, 0, 1, 1)
+        self.checkBox_2 = QtWidgets.QCheckBox(self.widget1)
+        self.checkBox_2.setMinimumSize(QtCore.QSize(120, 35))
+        self.checkBox_2.setObjectName("checkBox_2")
+        self.gridLayout_3.addWidget(self.checkBox_2, 0, 1, 1, 1)
+        self.checkBox = QtWidgets.QCheckBox(self.widget1)
+        self.checkBox.setMinimumSize(QtCore.QSize(120, 35))
+        self.checkBox.setObjectName("checkBox")
+        self.gridLayout_3.addWidget(self.checkBox, 1, 0, 1, 1)
+        self.verticalLayout_2.addWidget(self.groupBox_4)
+        self.groupBox_5 = QtWidgets.QGroupBox(self.widget)
+        self.groupBox_5.setMinimumSize(QtCore.QSize(371, 261))
         self.groupBox_5.setObjectName("groupBox_5")
-        self.label_2 = QtWidgets.QLabel(self.groupBox_5)
-        self.label_2.setGeometry(QtCore.QRect(20, 20, 54, 12))
+        self.textEdit_info_2 = QtWidgets.QTextEdit(self.groupBox_5)
+        self.textEdit_info_2.setGeometry(QtCore.QRect(10, 30, 341, 151))
+        self.textEdit_info_2.setObjectName("textEdit_info_2")
+        self.verticalLayout_2.addWidget(self.groupBox_5)
+        self.horizontalLayout_5.addLayout(self.verticalLayout_2)
+        self.groupBox_8 = QtWidgets.QGroupBox(self.widget)
+        self.groupBox_8.setMinimumSize(QtCore.QSize(372, 481))
+        self.groupBox_8.setObjectName("groupBox_8")
+        self.label = QtWidgets.QLabel(self.groupBox_8)
+        self.label.setGeometry(QtCore.QRect(30, 30, 81, 41))
+        self.label.setMinimumSize(QtCore.QSize(81, 41))
+        self.label.setObjectName("label")
+        self.comboBox_2 = QtWidgets.QComboBox(self.groupBox_8)
+        self.comboBox_2.setGeometry(QtCore.QRect(110, 40, 171, 30))
+        self.comboBox_2.setStyleSheet("QComboBox {\n"
+"    font-family: \"微软雅黑\";\n"
+"    color: black;\n"
+"    border: 1px #1a1a1a solid;\n"
+"    background-color:#62d8fe;\n"
+"}\n"
+"")
+        self.comboBox_2.setObjectName("comboBox_2")
+        self.comboBox_2.addItem("")
+        self.widget2 = QtWidgets.QWidget(self.groupBox_8)
+        self.widget2.setGeometry(QtCore.QRect(10, 120, 348, 348))
+        self.widget2.setObjectName("widget2")
+        self.gridLayout_5 = QtWidgets.QGridLayout(self.widget2)
+        self.gridLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_5.setObjectName("gridLayout_5")
+        self.label_2 = QtWidgets.QLabel(self.widget2)
+        self.label_2.setMinimumSize(QtCore.QSize(170, 170))
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
         self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(self.groupBox_5)
-        self.label_3.setGeometry(QtCore.QRect(20, 50, 291, 151))
+        self.gridLayout_5.addWidget(self.label_2, 0, 0, 1, 1)
+        self.label_3 = QtWidgets.QLabel(self.widget2)
+        self.label_3.setMinimumSize(QtCore.QSize(170, 170))
+        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
         self.label_3.setObjectName("label_3")
-        self.label_4 = QtWidgets.QLabel(self.groupBox_5)
-        self.label_4.setGeometry(QtCore.QRect(20, 220, 54, 12))
+        self.gridLayout_5.addWidget(self.label_3, 0, 1, 1, 1)
+        self.label_4 = QtWidgets.QLabel(self.widget2)
+        self.label_4.setMinimumSize(QtCore.QSize(170, 170))
+        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
         self.label_4.setObjectName("label_4")
-        self.label_5 = QtWidgets.QLabel(self.groupBox_5)
-        self.label_5.setGeometry(QtCore.QRect(20, 240, 141, 111))
+        self.gridLayout_5.addWidget(self.label_4, 1, 0, 1, 1)
+        self.label_5 = QtWidgets.QLabel(self.widget2)
+        self.label_5.setMinimumSize(QtCore.QSize(170, 170))
+        self.label_5.setAlignment(QtCore.Qt.AlignCenter)
         self.label_5.setObjectName("label_5")
-        self.label_6 = QtWidgets.QLabel(self.groupBox_5)
-        self.label_6.setGeometry(QtCore.QRect(180, 240, 141, 111))
-        self.label_6.setObjectName("label_6")
-        self.label_7 = QtWidgets.QLabel(self.groupBox_5)
-        self.label_7.setGeometry(QtCore.QRect(20, 360, 141, 111))
-        self.label_7.setObjectName("label_7")
-        self.label_8 = QtWidgets.QLabel(self.groupBox_5)
-        self.label_8.setGeometry(QtCore.QRect(180, 360, 141, 111))
-        self.label_8.setObjectName("label_8")
-        self.horizontalLayout_2.addWidget(self.groupBox_5)
-        self.horizontalLayout_2.setStretch(0, 2)
-        self.horizontalLayout_2.setStretch(1, 1)
-        self.horizontalLayout_2.setStretch(2, 2)
-        self.gridLayout_3.addLayout(self.horizontalLayout_2, 0, 0, 1, 1)
+        self.gridLayout_5.addWidget(self.label_5, 1, 1, 1, 1)
+        self.horizontalLayout_5.addWidget(self.groupBox_8)
         icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("E:/GitHub/pyQtTest/source/edit_image_128px_1169761_easyicon.net.ico"), QtGui.QIcon.Selected, QtGui.QIcon.On)
+        icon1.addPixmap(QtGui.QPixmap("source/edit_image_128px_1169761_easyicon.net.ico"), QtGui.QIcon.Selected, QtGui.QIcon.On)
         self.tabWidget.addTab(self.tab_process, icon1, "")
         self.tab_extract = QtWidgets.QWidget()
         self.tab_extract.setObjectName("tab_extract")
@@ -170,6 +280,32 @@ class Ui_MainWindow(object):
         self.horizontalLayout_3.addWidget(self.groupBox_6)
         self.extract = QtWidgets.QPushButton(self.tab_extract)
         self.extract.setMinimumSize(QtCore.QSize(101, 51))
+        self.extract.setStyleSheet("QPushButton{  \n"
+"    border: 1px solid #ffffff;    \n"
+"    background-color:#62d8fe;\n"
+"    border-style: solid;  \n"
+"    border-radius:5px;  \n"
+"    width: 100px;  \n"
+"    height:20px;  \n"
+"    padding:0 0px;  \n"
+"} \n"
+"QPushButton:hover{     \n"
+"    border: 1px solid #ffffff;  \n"
+"    background-color:#d6f7fe;\n"
+"    border-style: solid;  \n"
+"    border-radius:5px;  \n"
+"    width: 40px;  \n"
+"    height:20px;  \n"
+"    padding:0 0px;  \n"
+"}\n"
+"QPushButton:pressed{  \n"
+"    background-color:#EAF0FF;  \n"
+"    border: 1px solid #AAB4C4;  \n"
+"    width: 40px;  \n"
+"    height:20px;  \n"
+"    padding:0 0px;  \n"
+"    border-radius:5px;  \n"
+"}")
         self.extract.setObjectName("extract")
         self.horizontalLayout_3.addWidget(self.extract)
         self.groupBox_7 = QtWidgets.QGroupBox(self.tab_extract)
@@ -184,18 +320,19 @@ class Ui_MainWindow(object):
         self.horizontalLayout_3.setStretch(2, 2)
         self.gridLayout_4.addLayout(self.horizontalLayout_3, 0, 0, 1, 1)
         icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap("E:/GitHub/pyQtTest/source/image_128px_1197371_easyicon.net.ico"), QtGui.QIcon.Selected, QtGui.QIcon.On)
+        icon2.addPixmap(QtGui.QPixmap("source/image_128px_1197371_easyicon.net.ico"), QtGui.QIcon.Selected, QtGui.QIcon.On)
         self.tabWidget.addTab(self.tab_extract, icon2, "")
         self.tab_recognize = QtWidgets.QWidget()
         self.tab_recognize.setObjectName("tab_recognize")
         icon3 = QtGui.QIcon()
-        icon3.addPixmap(QtGui.QPixmap("E:/GitHub/pyQtTest/source/detective_128px_1168533_easyicon.net.ico"), QtGui.QIcon.Selected, QtGui.QIcon.On)
+        icon3.addPixmap(QtGui.QPixmap("source/detective_128px_1168533_easyicon.net.ico"), QtGui.QIcon.Selected, QtGui.QIcon.On)
         self.tabWidget.addTab(self.tab_recognize, icon3, "")
         self.gridLayout.addWidget(self.tabWidget, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
         self.tabWidget.setCurrentIndex(0)
+        self.tabWidget_2.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
@@ -211,20 +348,31 @@ class Ui_MainWindow(object):
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">选择文件夹名称：</p></body></html>"))
         self.groupBox.setTitle(_translate("MainWindow", "预览"))
         self.image_preshow.setText(_translate("MainWindow", "选择预览的图片："))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_file), _translate("MainWindow", "打开文件"))
-        self.groupBox_4.setTitle(_translate("MainWindow", "数据增强"))
-        self.checkBox.setText(_translate("MainWindow", "随机裁剪"))
-        self.checkBox_2.setText(_translate("MainWindow", "尺度变换"))
+        self.comboBox.setCurrentText(_translate("MainWindow", "暂无"))
+        self.comboBox.setItemText(0, _translate("MainWindow", "暂无"))
+        self.label_show.setText(_translate("MainWindow", "暂无图片"))
+        self.tabWidget_2.setTabText(self.tabWidget_2.indexOf(self.tab), _translate("MainWindow", "原图"))
+        self.label_show_gray.setText(_translate("MainWindow", "暂无图片"))
+        self.tabWidget_2.setTabText(self.tabWidget_2.indexOf(self.tab_2), _translate("MainWindow", "灰度图"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_file), _translate("MainWindow", "载入数据"))
+        self.groupBox_4.setTitle(_translate("MainWindow", "数据扩充"))
+        self.pushButtoncheck.setText(_translate("MainWindow", "确定"))
         self.checkBox_3.setText(_translate("MainWindow", "旋转变换"))
-        self.pushButton_check.setText(_translate("MainWindow", "确定"))
-        self.groupBox_5.setTitle(_translate("MainWindow", "预览"))
-        self.label_2.setText(_translate("MainWindow", "原图："))
-        self.label_3.setText(_translate("MainWindow", "原图"))
-        self.label_4.setText(_translate("MainWindow", "效果图："))
-        self.label_5.setText(_translate("MainWindow", "1"))
-        self.label_6.setText(_translate("MainWindow", "2"))
-        self.label_7.setText(_translate("MainWindow", "3"))
-        self.label_8.setText(_translate("MainWindow", "4"))
+        self.checkBox_2.setText(_translate("MainWindow", "尺度变换"))
+        self.checkBox.setText(_translate("MainWindow", "随机裁剪"))
+        self.groupBox_5.setTitle(_translate("MainWindow", "信息显示"))
+        self.textEdit_info_2.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">暂无</p></body></html>"))
+        self.groupBox_8.setTitle(_translate("MainWindow", "预览"))
+        self.label.setText(_translate("MainWindow", "扩充方式："))
+        self.comboBox_2.setItemText(0, _translate("MainWindow", "请选择"))
+        self.label_2.setText(_translate("MainWindow", "暂无"))
+        self.label_3.setText(_translate("MainWindow", "暂无"))
+        self.label_4.setText(_translate("MainWindow", "暂无"))
+        self.label_5.setText(_translate("MainWindow", "暂无"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_process), _translate("MainWindow", "预处理"))
         self.groupBox_6.setTitle(_translate("MainWindow", "特征选择"))
         self.checkBox_FD.setText(_translate("MainWindow", "FD形状特征"))
